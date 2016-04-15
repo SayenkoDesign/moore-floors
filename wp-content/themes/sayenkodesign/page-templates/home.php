1a1d79631d5142b56cc8f62b7661a058044e5e15
Robert M. Parker <rparker@yamiko.ninja>
2016-04-15 02:42:57 -0700

    why section

--- wp-content/themes/sayenkodesign/page-templates/home.php
+++ wp-content/themes/sayenkodesign/page-templates/home.php
@@ -37,6 +37,19 @@ while (have_rows('content')) {
             ];
             $flexibleContent[] = $twig->render('partials/services.html.twig', $data);
             break;
+        case 'why':
+            $accordion = $twig->render('panels/accordion.html.twig', ['accordion' => get_sub_field('accordion')]);
+            $data = [
+                'icon' => get_sub_field('icon'),
+                'title_1' => get_sub_field('title_line_1'),
+                'title_2' => get_sub_field('title_line_2'),
+                'accordion' => $accordion,
+                'testimonial_quote' => get_sub_field('testimony_text'),
+                'testimonial_author' => get_sub_field('testimony_author'),
+                'background' => get_sub_field('background_image'),
+            ];
+            $flexibleContent[] = $twig->render('partials/why.html.twig', $data);
+            break;
         default:
             throw new \Exception("Template does not support a layout for $layout");
             break;
