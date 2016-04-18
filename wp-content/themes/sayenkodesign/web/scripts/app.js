a6e663ba9b497f488b43b873549758a0baddf089
Robert M. Parker <rparker@yamiko.ninja>
2016-04-17 18:35:52 -0700

    services and h1

--- wp-content/themes/sayenkodesign/web/scripts/app.js
+++ wp-content/themes/sayenkodesign/web/scripts/app.js
@@ -1,5 +1,11 @@
 jQuery(function() {
     jQuery(document).foundation();
-    jQuery('.slick').slick({
+    jQuery('.slick').slick({});
+    jQuery('.warranty img').on('click', function(){
+        console.log('click');
+        jQuery(this).prev('.tooltip').toggleClass('hide');
+    });
+    jQuery('.warranty .tooltip').on('click', function(){
+        jQuery(this).toggleClass('hide');
     });
 });
